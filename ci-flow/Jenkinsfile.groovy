a38adb9d5d37e29a1777c8a0ade4663da63e82da
Bohdan Ponomarev <livenonex@gmail.com>
2020-08-28 18:44:49 +0300

    Try to verify what is wrong

--- ci-flow/Jenkinsfile.groovy
+++ ci-flow/Jenkinsfile.groovy
@@ -19,8 +19,7 @@ pipeline {
         }
         stage('Init and migrate databse') {
             steps {
-                sh 'mysql -u ${DB_USERNAME} -p${DB_PASSWORD} -h ${DB_URL} -P 3306'
-                sh 'source ./db_init.sql'
+                sh 'mysql -u ${DB_USERNAME} -p${DB_PASSWORD} -h ${DB_URL} -P 3306 < ./db_init.sql'
             }
         }
         stage('Integration tests') {
